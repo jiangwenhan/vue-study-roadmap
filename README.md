12e00e4ac7c905cf7648f97f2808c30796c78ab7
Wenhan Jiang <jiangwenhan@snda.com>
2019-03-28 20:03:48 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# vue-study-roadmap
-Created with CodeSandbox
---
+++ src/App2.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app2">
+    <p>
+      <span v-bind:title="message">鼠标悬停几秒钟查看此处动态绑定的提示信息！</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App2",
+  data: function() {
+    return {
+      message: "页面加载于 " + new Date().toLocaleString()
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
